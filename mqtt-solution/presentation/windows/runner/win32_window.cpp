34ef707daf8c004e97655d3dc44965f143871a10
Barcagiul <Ethan.kn@protonmail.com>
2025-10-17 10:13:43 +0100

    add min window size for windows device

--- mqtt-solution/presentation/windows/runner/win32_window.cpp
+++ mqtt-solution/presentation/windows/runner/win32_window.cpp
@@ -216,6 +216,14 @@ Win32Window::MessageHandler(HWND hwnd,
     case WM_DWMCOLORIZATIONCOLORCHANGED:
       UpdateTheme(hwnd);
       return 0;
+
+    case WM_GETMINMAXINFO: {
+      // Set minimum window size to prevent overflow issues 
+      LPMINMAXINFO lpMMI = (LPMINMAXINFO)lparam;
+      lpMMI->ptMinTrackSize.x = 800;
+      lpMMI->ptMinTrackSize.y = 600;
+      return 0;
+    }
   }
 
   return DefWindowProc(window_handle_, message, wparam, lparam);
